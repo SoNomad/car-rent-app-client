5ae7527d75c0a5786184c1a6ee30b2647f4d9772
SoNomad <nurmuhammad16203@gmail.com>
2023-02-10 10:14:23 +0300

    switcher added

--- src/components/carSwitcher/carSwitcher.jsx
+++ src/components/carSwitcher/carSwitcher.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import styles from './carSwitcher.module.scss';
+import React, { useState } from 'react';
+import styles from './CarSwitch.module.scss';
+import car from './car.png';
+import car2 from './car2.png';
+import flot from './flot.png';
 
-const carSwitcher = () => {
-  return <div></div>;
+const CarSwitcher = () => {
+  const [active, setActive] = useState('');
+
+  const classHandler = (item) => {
+    setActive((prev) => (prev = item));
+  };
+
+  return (
+    <div className={styles.items}>
+      <div
+        className={active === 'car' ? styles.active : styles.inActive}
+        onClick={() => classHandler('car')}
+      >
+        <img src={car} alt="" height={50} />
+      </div>
+      <div
+        className={active === 'jeep' ? styles.active : styles.inActive}
+        onClick={() => classHandler('jeep')}
+      >
+        <img src={car2} alt="" height={50} />
+      </div>
+      <div
+        className={active === 'ship' ? styles.active : styles.inActive}
+        onClick={() => classHandler('ship')}
+      >
+        <img src={flot} alt="" height={50} />
+      </div>
+    </div>
+  );
 };
 
-export default carSwitcher;
+export default CarSwitcher;
