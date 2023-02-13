b5064ece9f11f900e779936f27f24f2fec28af66
Ibragim Bakaev <raininsnow@Bbg.local>
2023-02-13 17:23:06 +0300

    Начало работы с Conditions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { Route, Routes } from 'react-router';
-import ContactsBar from './components/ContactsBar/ContactsBar';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import ManagerChat from './components/ManagerChat/ManagerChat';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Blog from './pages/Blog/Blog';
-import Conditions from './pages/Conditions/Conditions';
-import Contacts from './pages/Contacts/Contacts';
-import Home from './pages/Home/Home';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import { Route, Routes } from "react-router";
+import ContactsBar from "./components/ContactsBar/ContactsBar";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import ManagerChat from "./components/ManagerChat/ManagerChat";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Blog from "./pages/Blog/Blog";
+import Conditions from "./pages/Conditions/Conditions";
+import Contacts from "./pages/Contacts/Contacts";
+import Home from "./pages/Home/Home";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ContactsBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/conditions" element={<Conditions />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/conditions" element={<Conditions />} />
+        </Routes>
+      </Layout>
       <ManagerChat />
       <Footer />
     </div>
--- src/pages/Conditions/Conditions.jsx
+++ src/pages/Conditions/Conditions.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React from "react";
+import styles from "./Conditions.module.scss";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+// import "react-accessible-accordion/dist/fancy-example.css";
 
 const Conditions = () => {
-  return <div></div>;
+  const conditions = [
+    {
+      heading: "Что нужно иметь при получении автомобиля?",
+      content: "conten1",
+    },
+    {
+      heading:
+        "Нужно ли международное водительское удостоверение для аренды автомобиля?",
+      content:
+        "Для аренды автомобиля в MAKS Car Rental достаточно НВУ (национальное водительское удостоверение). При ДТП они являются действительными для страховой компании, в которой застрахованы наши автомобили. Однако, рекомендуем получить МВУ (международное водительское удостоверение), в связи с участившимися случаями претензий со стороны дорожной полиции Таиланда.",
+    },
+    { heading: "Как забронировать автомобиль?", content: "content3" },
+    {
+      heading: "Каков минимальный возраст для аренды автомобиля?",
+      content: "conten1",
+    },
+    {
+      heading: "Что нужно иметь при получении автомобиля?",
+      content: "conten1",
+    },
+  ];
+
+  return (
+    <div className={styles.conditionsWrapper}>
+      <h1 className={styles.title}>Условия аренды</h1>
+      <div>
+        <Accordion allowZeroExpanded className={styles.conditionsContainer}>
+          {conditions.map((item) => (
+            <AccordionItem key={item.uuid}>
+              <hr className={styles.hr} />
+              <AccordionItemHeading className={styles.accordHeding}>
+                <AccordionItemButton>{item.heading}</AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel className={styles.accordContent}>
+                {item.content}
+              </AccordionItemPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </div>
+  );
 };
 
 export default Conditions;
