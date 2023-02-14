5f713f8b297cb877d0874416b2599e85d18839c7
Ibragim Bakaev <raininsnow@Bbg.local>
2023-02-14 17:35:51 +0300

    Продолжение...

--- src/pages/Conditions/Conditions.jsx
+++ src/pages/Conditions/Conditions.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Conditions.module.scss";
+import { ReactComponent as ArrowIcon } from "./arrow.svg";
 import {
   Accordion,
   AccordionItem,
@@ -10,25 +11,38 @@ import {
 // import "react-accessible-accordion/dist/fancy-example.css";
 
 const Conditions = () => {
+  const [isRotated, setIsRotated] = useState(false);
+
+  const handleClick = () => {
+    setIsRotated(!isRotated);
+  };
+
   const conditions = [
     {
       heading: "Что нужно иметь при получении автомобиля?",
-      content: "conten1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nibh ipsum. Maecenas faucibus eros sed magna ultricies, eu suscipit sapien suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eget pretium justo. Sed quis leo euismod, consectetur dui eu, iaculis nunc. Quisque at massa a augue aliquet dignissim.",
     },
     {
       heading:
         "Нужно ли международное водительское удостоверение для аренды автомобиля?",
       content:
-        "Для аренды автомобиля в MAKS Car Rental достаточно НВУ (национальное водительское удостоверение). При ДТП они являются действительными для страховой компании, в которой застрахованы наши автомобили. Однако, рекомендуем получить МВУ (международное водительское удостоверение), в связи с участившимися случаями претензий со стороны дорожной полиции Таиланда.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nibh ipsum. Maecenas faucibus eros sed magna ultricies, eu suscipit sapien suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eget pretium justo. Sed quis leo euismod, consectetur dui eu, iaculis nunc. Quisque at massa a augue aliquet dignissim.",
     },
-    { heading: "Как забронировать автомобиль?", content: "content3" },
+    {
+      heading: "Как забронировать автомобиль?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nibh ipsum. Maecenas faucibus eros sed magna ultricies, eu suscipit sapien suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eget pretium justo. Sed quis leo euismod, consectetur dui eu, iaculis nunc. Quisque at massa a augue aliquet dignissim.",
+    },
     {
       heading: "Каков минимальный возраст для аренды автомобиля?",
-      content: "conten1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nibh ipsum. Maecenas faucibus eros sed magna ultricies, eu suscipit sapien suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eget pretium justo. Sed quis leo euismod, consectetur dui eu, iaculis nunc. Quisque at massa a augue aliquet dignissim.",
     },
     {
       heading: "Что нужно иметь при получении автомобиля?",
-      content: "conten1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nibh ipsum. Maecenas faucibus eros sed magna ultricies, eu suscipit sapien suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eget pretium justo. Sed quis leo euismod, consectetur dui eu, iaculis nunc. Quisque at massa a augue aliquet dignissim.",
     },
   ];
 
@@ -40,8 +54,15 @@ const Conditions = () => {
           {conditions.map((item) => (
             <AccordionItem key={item.uuid}>
               <hr className={styles.hr} />
-              <AccordionItemHeading className={styles.accordHeding}>
-                <AccordionItemButton>{item.heading}</AccordionItemButton>
+              <AccordionItemHeading onClick={handleClick} className={styles.accordHeading}>
+                <AccordionItemButton
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  {item.heading}
+                  <ArrowIcon
+                    style={{ transform: isRotated ? "rotate(180deg)" : "none" }}
+                  />
+                </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className={styles.accordContent}>
                 {item.content}
