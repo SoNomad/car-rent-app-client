55aea7e7ceba66c67401bfde68d37eae25a00963
SoNomad <nurmuhammad16203@gmail.com>
2023-02-17 22:04:10 +0300

    добавлены датапиккер и выбор локации

--- src/components/carSwitcher/carSwitcher.jsx
+++ src/components/carSwitcher/carSwitcher.jsx
@@ -12,24 +12,31 @@ const CarSwitcher = () => {
   };
 
   return (
-    <div className={styles.items}>
-      <div
-        className={active === 'car' ? styles.active : styles.inActive}
-        onClick={() => classHandler('car')}
-      >
-        <img src={car} alt="" height={50} />
+    <div>
+      <div className={styles.items}>
+        <div
+          className={active === 'car' ? styles.active : styles.inActive}
+          onClick={() => classHandler('car')}
+        >
+          <img src={car} alt="" height={50} />
+        </div>
+        <div
+          className={active === 'jeep' ? styles.active : styles.inActive}
+          onClick={() => classHandler('jeep')}
+        >
+          <img src={car2} alt="" height={50} />
+        </div>
+        <div
+          className={active === 'ship' ? styles.active : styles.inActive}
+          onClick={() => classHandler('ship')}
+        >
+          <img src={flot} alt="" height={50} />
+        </div>
       </div>
-      <div
-        className={active === 'jeep' ? styles.active : styles.inActive}
-        onClick={() => classHandler('jeep')}
-      >
-        <img src={car2} alt="" height={50} />
-      </div>
-      <div
-        className={active === 'ship' ? styles.active : styles.inActive}
-        onClick={() => classHandler('ship')}
-      >
-        <img src={flot} alt="" height={50} />
+      <div className={styles.subtitle}>
+        <div className={styles.item}>Авто</div>
+        <div className={styles.item}>Мото</div>
+        <div className={styles.item}>Катеры</div>
       </div>
     </div>
   );
