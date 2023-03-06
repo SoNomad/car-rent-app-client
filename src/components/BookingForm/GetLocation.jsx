a75a8dad80bc0ddebafdf4762bd6ad1a130e20a8
SoNomad <nurmuhammad16203@gmail.com>
2023-03-06 11:41:55 +0300

    Изменены точки на карте

--- src/components/BookingForm/GetLocation.jsx
+++ src/components/BookingForm/GetLocation.jsx
@@ -4,26 +4,26 @@ import { Dropdown, Space } from 'antd';
 
 const GetLocation = ({ placeHolder, setPlaceHolder }) => {
   const [getLocation, setGetLocation] = useState(
-    'https://maps.google.com/maps?q=%D0%B3%D1%80%D0%BE%D0%B7%D0%BD%D1%8B%D0%B9&t=&z=13&ie=UTF8&iwloc=&output=embed'
+    'https://maps.google.com/maps?q=%D1%81%D0%B0%D0%BC%D1%83%D0%B8%20%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82&t=&z=13&ie=UTF8&iwloc=&output=embed'
   );
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleMenuClick = (e) => {
     setPlaceHolder(e.key);
     setOpen(true);
-    if (e.key === 'Аэропорт Грозного') {
+    if (e.key === 'Аэропорт Самуи') {
       setGetLocation(
-        'https://maps.google.com/maps?q=grozny%20aeroport&t=&z=13&ie=UTF8&iwloc=&output=embed'
+        'https://maps.google.com/maps?q=%D1%81%D0%B0%D0%BC%D1%83%D0%B8%20%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82&t=&z=13&ie=UTF8&iwloc=&output=embed'
       );
     }
-    if (e.key === 'Комплекс "Грозный Сити"') {
+    if (e.key === 'Пляж "Тонгсон Бэй') {
       setGetLocation(
-        'https://maps.google.com/maps?q=grozny%20%D0%BE%D1%82%D0%B5%D0%BB%D1%8C%20%D0%B3%D1%80%D0%BE%D0%B7%D0%BD%D1%8B%D0%B9%20%D1%81%D0%B8%D1%82%D0%B8&t=&z=13&ie=UTF8&iwloc=&output=embed'
+        'https://maps.google.com/maps?q=%D1%81%D0%B0%D0%BC%D1%83%D0%B8%20%D0%BF%D0%BB%D1%8F%D0%B6%20%D1%82%D0%BE%D0%BD%D0%B3%D1%81%D0%BE%D0%BD&t=&z=13&ie=UTF8&iwloc=&output=embed'
       );
     }
-    if (e.key === 'Офис "Интукод"') {
+    if (e.key === 'Оки Таун') {
       setGetLocation(
-        'https://maps.google.com/maps?q=grozny%20intocode&t=&z=13&ie=UTF8&iwloc=&output=embed'
+        'https://maps.google.com/maps?q=%D1%81%D0%B0%D0%BC%D1%83%D0%B8%20oki%20town&t=&z=15&ie=UTF8&iwloc=&output=embed'
       );
     }
   };
@@ -34,16 +34,16 @@ const GetLocation = ({ placeHolder, setPlaceHolder }) => {
 
   const items = [
     {
-      label: <span>Аэропорт Грозного</span>,
-      key: 'Аэропорт Грозного',
+      label: <span>Аэропорт Самуи</span>,
+      key: 'Аэропорт Самуи',
     },
     {
-      label: <span>Комплекс "Грозный Сити""</span>,
-      key: 'Комплекс "Грозный Сити"',
+      label: <span>Пляж "Тонгсон Бэй"</span>,
+      key: 'Пляж "Тонгсон Бэй',
     },
     {
-      label: <span>Офис "Интукод"</span>,
-      key: 'Офис "Интукод"',
+      label: <span>Оки Таун</span>,
+      key: 'Оки Таун',
     },
     {
       label: (
