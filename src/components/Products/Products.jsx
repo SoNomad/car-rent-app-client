3019f84592e293b61ff7435d43d6c1cf61127e15
SoNomad <nurmuhammad16203@gmail.com>
2023-03-12 20:52:31 +0300

    Добавил пагинацию

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,48 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Products.module.scss';
 import { Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { brown } from '@mui/material/colors';
-import { orange } from '@mui/material/colors';
+
 import BookingConfirm from '../BookingConfirm/BookingConfirm';
 import axios from '../../app/axios';
 import { useDispatch } from 'react-redux';
 import { checkReserved } from '../../app/slices/bookingSlice';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const Products = ({ placeHolder, fromDate, toDate }) => {
   //стилизация кнопки MUI
-  const ColorButton = styled(Button)(({ theme }) => ({
-    color: theme.palette.getContrastText(brown[500]),
-    backgroundColor: orange[500],
-    '&:hover': {
-      backgroundColor: orange[700],
-    },
-  }));
 
   const dispatch = useDispatch();
   const [show, setShow] = useState(false); //стэйт для показа окна заказа
-  const [data, setData] = useState(); // для даты с календаря
+  const [data, setData] = useState(); // список машин с базы
   const [isLoading, setLoading] = useState(true); // для лоадера
   const [carId, setCarId] = useState(''); //для передачи айди в кнопку брони
+
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   //фетч списка авто
   useEffect(() => {
     axios
-      .get('/car')
+      .get(`/car?page=${page}`)
       .then((res) => {
-        setData(res.data);
-        console.log(res.data);
+        console.log(res);
+        setData(res.data.cars);
+        setPageCount(res.data.pageCount);
         setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [page]);
 
   //хэндлер для отправки конкретного авто в окно заказа и открытия окна
-  const handleBooking = async (carById) => {
+  const handleBooking = (carById) => {
     const params = { _carId: carById._id, fromDate, toDate };
-    await dispatch(checkReserved(params));
+    dispatch(checkReserved(params));
     setShow(!show);
     setCarId(carById);
   };
@@ -54,62 +55,67 @@ const Products = ({ placeHolder, fromDate, toDate }) => {
 
   //категории
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.catPanel}>
-        <h4>Легковые</h4>
-        <h4>Внедорожники</h4>
-        <h4>Минивены</h4>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        {/* открытие кона заказа и передача в него информации о датах и адресе */}
+        {show && (
+          <BookingConfirm
+            setShow={setShow}
+            car={carId}
+            fromDate={fromDate}
+            toDate={toDate}
+            location={placeHolder}
+          />
+        )}
+        {/* маппинг списка полученных авто */}
+        <div className={styles.itemContainer}>
+          {data.map((car) => (
+            <div className={styles.item} key={car._id}>
+              <div className={styles.imageBlock}>
+                <img src={car.imageUrl} alt="" height={160} />
+              </div>
+              <div className={styles.itemInfo}>
+                <h2 style={{ fontWeight: 400, fontSize: 34 }}>{car.name}</h2>
+                <div className={styles.details}>
+                  <div className={styles.paymentInfo}>
+                    <h1 style={{ fontWeight: 400 }}>{car.payPerDay}</h1>
+                    <h3>RUB/day</h3>
+                    <p>При аренде на 30 дней</p>
+                    <p>30 дней / {car.payPerDay * 30} RUB</p>
+                  </div>
+                  <div className={styles.desc}>
+                    <ul>
+                      <li>Автоматич КПП</li>
+                      <li>{car.engine}</li>
+                      <li>Кондиционер</li>
+                      <li>{car.seats}</li>
+                      <li>Парковочные датчики</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <Button
+                    variant="contained"
+                    disabled={fromDate && placeHolder !== 'Место получения' ? false : true}
+                    onClick={() => handleBooking(car)}
+                  >
+                    Забронировать
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-
-      {/* открытие кона заказа и передача в него информации о датах и адресе */}
-      {show && (
-        <BookingConfirm
-          setShow={setShow}
-          car={carId}
-          fromDate={fromDate}
-          toDate={toDate}
-          location={placeHolder}
+      <Stack spacing={2}>
+        <Pagination
+          count={pageCount}
+          page={page}
+          onChange={handleChange}
+          showFirstButton
+          showLastButton
         />
-      )}
-      {/* маппинг списка полученных авто */}
-      <div className={styles.itemContainer}>
-        {data.map((car) => (
-          <div className={styles.item} key={car._id}>
-            <div className={styles.imageBlock}>
-              <img src={car.imageUrl} alt="" height={120} />
-            </div>
-            <div className={styles.itemInfo}>
-              <h2 style={{ fontWeight: 400, fontSize: 34 }}>{car.name}</h2>
-              <div className={styles.details}>
-                <div className={styles.paymentInfo}>
-                  <h1 style={{ fontWeight: 400 }}>{car.payPerDay}</h1>
-                  <h3>RUB/day</h3>
-                  <p>При аренде на 30 дней</p>
-                  <p>30 дней / {car.payPerDay * 30} RUB</p>
-                </div>
-                <div className={styles.desc}>
-                  <ul>
-                    <li>Автоматич КПП</li>
-                    <li>{car.engine}</li>
-                    <li>Кондиционер</li>
-                    <li>{car.seats}</li>
-                    <li>Парковочные датчики</li>
-                  </ul>
-                </div>
-              </div>
-              <div>
-                <ColorButton
-                  variant="contained"
-                  disabled={fromDate && placeHolder !== 'Место получения' ? false : true}
-                  onClick={() => handleBooking(car)}
-                >
-                  Забронировать
-                </ColorButton>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      </Stack>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import BookingForm from '../../components/BookingForm/BookingForm';
 import Products from '../../components/Products/Products';
 import CarSwitcher from '../../components/carSwitcher/carSwitcher';
 import Section from '../../components/Section/Section';
+import clouds from './cloud.png';
+import gradient from './gradient.png';
 
 const Home = ({ setIsTrue }) => {
   setIsTrue(true);
@@ -15,6 +17,17 @@ const Home = ({ setIsTrue }) => {
   return (
     <>
       <div className={styles.header}>
+        <marquee
+          direction="right"
+          behavior="alternate"
+          width="100%"
+          height="100%"
+          scrollamount="5"
+          className={styles.clouds}
+        >
+          <img src={clouds}></img>
+        </marquee>
+        <img src={gradient} width="100%" className={styles.gradient} />
         <div className={styles.logoName}>
           <Typography mr={2} pr={2} variant="h1" sx={{ color: 'white', fontWeight: 400 }}>
             Самуи
